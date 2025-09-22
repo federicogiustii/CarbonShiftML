597668a2e5edf01033690892d1ef783b217344a4
federicogiustii <153283171+federicogiustii@users.noreply.github.com>
2025-09-22 10:46:44 +0200

    Update client_callback_ML.py

--- client_callback_ML.py
+++ client_callback_ML.py
@@ -1,17 +1,18 @@
+
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/callback", methods=["POST"])
 def callback():
-    data = request.json
-
-    print("\n[CLIENT] Callback ricevuta:")
-    print(f"    • Task: {data.get('task', 'Unknown')}")
-    print(f"    • Strategia: {data.get('strategy', 'Unknown')}")
-    print(f"    • Slot eseguito: {data.get('slot_executed', 'Unknown')}")
-    print(f"    • Output: {data.get('result', 'N/A')}")
-
+    data = request.get_json()
+    print(f"""
+[CLIENT] Callback ricevuta:
+    • Task: {data.get("task", "Echo")}
+    • Strategia: {data.get("strategy", "low")}
+    • Slot eseguito: {data.get("slot_executed", "-1")}
+    • Output: {data.get("result", "[Nessun risultato]")}
+    """)
     return "OK", 200
 
 if __name__ == "__main__":
